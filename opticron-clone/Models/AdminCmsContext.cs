34e4398a611d17aa2625e7576d565abbebbfe086
Ian Scott <jaycossey@live.com>
2024-04-22 00:34:02 +0100

    added navbar controller and comments for planning, need to compartmentalise views and setup models for db

--- opticron-clone/Models/AdminCmsContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace AdminCms.Models;
-
-public class AdminCmsContext
-{
-    public int Id { get; set; }
-    public string? Title { get; set; }
-}
