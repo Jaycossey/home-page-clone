c28684937ca7977c91de692ee5d42826f8d6e5a4
Ian Scott <jaycossey@live.com>
2024-04-23 13:07:07 +0100

    commit before push to origin

--- opticron-clone/obj/Debug/net8.0/opticron-clone.AssemblyInfo.cs
+++ opticron-clone/obj/Debug/net8.0/opticron-clone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("opticron-clone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9805e25acd5f8a3c5422e53c635fe64dac60cb35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efca7840d3cbe2d4e410db5c6e9b5da49c3e54e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("opticron-clone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("opticron-clone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
